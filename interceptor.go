483e99dfa564785e39551961e96c2fd1aff004e3
Tom Bojer <t.bojer@shopware.com>
2025-08-19 11:43:59 +0200

    feat: check usable trace from WithPropagateResponseHeader
    Signed-off-by: Tom Bojer <t.bojer@shopware.com>

--- interceptor.go
+++ interceptor.go
@@ -160,7 +160,7 @@ func (i *Interceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
 				responseSize = proto.Size(msg)
 			}
 			span.SetAttributes(headerAttributes(protocol, responseKey, response.Header(), i.config.responseHeaderKeys)...)
-			if !isClient && i.config.injectTraceParentResponseHeader {
+			if !isClient && i.config.propagateResponseHeader {
 				responseCarrier := propagation.HeaderCarrier(response.Header())
 				i.config.propagator.Inject(ctx, responseCarrier)
 			}
@@ -317,13 +317,13 @@ func (i *Interceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) co
 			traceOpts...,
 		)
 		defer span.End()
-		
+
 		// Inject traceparent into response headers if enabled
-		if i.config.injectTraceParentResponseHeader {
+		if i.config.propagateResponseHeader {
 			responseCarrier := propagation.HeaderCarrier(conn.ResponseHeader())
 			i.config.propagator.Inject(ctx, responseCarrier)
 		}
-		
+
 		streamingHandler := &streamingHandlerInterceptor{
 			StreamingHandlerConn: conn,
 			receive: func(msg any, conn connect.StreamingHandlerConn) error {
